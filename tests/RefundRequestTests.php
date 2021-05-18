e454ffe8e54b103533bf46da0e4dab13657cc8d1
rahat@ssd-tech.com <rahat@ssd-tech.com>
2021-05-18 18:49:02 +0600

    dummy test push

--- tests/RefundRequestTests.php
+++ tests/RefundRequestTests.php
@@ -24,18 +24,15 @@ class RefundRequestTests extends TestCase
 
     /**
      * @test
-     * @param bool $is_return
-     * @return array
-     * @throws JsonException
      */
-    final public function fetch_payment_list($is_return = false): array
+    final public function fetch_payment_list(): array
     {
         $paymentListRequestUrl = 'https://sb-payments.ghoori.com.bd/api/v1.0/subscription/310/payments'; //replace SERVER_URL & id (cancel request id) with value
         $paymentListRequest = PaymentListRequest::getInstance($paymentListRequestUrl, $this->fetch_access_token());
         $response = $paymentListRequest->send();
-        if ($is_return) {
+        /*if ($is_return) {
             return $response['subscriptionPayments'];
-        }
+        }*/
 
         self::assertNotEmpty($response);
         self::assertArrayHasKey('subscriptionPayments', $response);
@@ -65,114 +62,9 @@ class RefundRequestTests extends TestCase
             self::assertNotEmpty($subscriptionPayment['transactionId']);
         }
 
-        return [];
+        //return [];
     }
 
-    /**
-     * @test
-     * @throws JsonException
-     */
-    final public function fetch_payment_details(): void
-    {
-        $subscriptionPayments = $this->fetch_payment_list(true);
-        self::assertNotEmpty($subscriptionPayments);
-
-        foreach ($subscriptionPayments as $subscriptionPayment) {
-            $paymentDetailsRequestUrl = "https://sb-payments.ghoori.com.bd/api/v1.0/subscription/payment/{$subscriptionPayment['paymentId']}";
-            $paymentDetailsRequest = PaymentDetailsRequest::getInstance($paymentDetailsRequestUrl, $this->fetch_access_token());
-            $paymentDetails = $paymentDetailsRequest->send();
-
-            self::assertNotEmpty($paymentDetails);
-            self::assertArrayHasKey('paymentId', $paymentDetails);
-            self::assertArrayHasKey('cycle', $paymentDetails);
-            self::assertArrayHasKey('dueDate', $paymentDetails);
-            self::assertArrayHasKey('reverseTransactionAmount', $paymentDetails);
-            self::assertArrayHasKey('reverseTransactionDate', $paymentDetails);
-            self::assertArrayHasKey('reverseTransactionId', $paymentDetails);
-            self::assertArrayHasKey('status', $paymentDetails);
-            self::assertArrayHasKey('subscriptionId', $paymentDetails);
-            self::assertArrayHasKey('transactionDate', $paymentDetails);
-            self::assertArrayHasKey('transactionId', $paymentDetails);
-
-            self::assertNotEmpty($paymentDetails['paymentId']);
-            self::assertNotEmpty($paymentDetails['cycle']);
-            self::assertNotEmpty($paymentDetails['dueDate']);
-            self::assertNotEmpty($paymentDetails['reverseTransactionAmount']);
-            self::assertNotEmpty($paymentDetails['reverseTransactionDate']);
-            self::assertNotEmpty($paymentDetails['reverseTransactionId']);
-            self::assertNotEmpty($paymentDetails['status']);
-            self::assertNotEmpty($paymentDetails['subscriptionId']);
-            self::assertNotEmpty($paymentDetails['transactionDate']);
-            self::assertNotEmpty($paymentDetails['transactionId']);
-        }
-    }
-
-    /**
-     * @test
-     */
-    final public function send_refund_request(): void
-    {
-        $subscriptionPayments = $this->fetch_payment_list(true);
-        self::assertNotEmpty($subscriptionPayments);
-
-        foreach ($subscriptionPayments as $subscriptionPayment) {
-            $paymentRefundRequestUrl = "https://sb-payments.ghoori.com.bd/api/v1.0/subscription/payment/{$subscriptionPayment['paymentId']}/refund";
-            $refund_amount = 1;
-            $paymentRefundRequest = \Dotlines\GhooriSubscription\PaymentRefundRequest::getInstance($paymentRefundRequestUrl, $this->fetch_access_token(), $refund_amount);
-            $response = $paymentRefundRequest->send();
-
-            self::assertNotEmpty($response);
-
-            if (Carbon::now()->diffInDays($subscriptionPayment['transactionDate']) >= 15) {
-                self::assertArrayHasKey('errorCode', $response);
-                self::assertArrayHasKey('errorMessage', $response);
-
-                self::assertNotEmpty($response['errorCode']);
-                self::assertNotEmpty($response['errorMessage']);
-            } else {
-                self::assertArrayHasKey('requestID', $response);
-                self::assertArrayHasKey('amount', $response);
-                self::assertArrayHasKey('status', $response);
-                self::assertArrayHasKey('errorCode', $response);
-                self::assertArrayHasKey('errorMessage', $response);
-
-                self::assertNotEmpty($response['requestID']);
-                self::assertNotEmpty($response['amount']);
-                self::assertNotEmpty($response['status']);
-                self::assertNotEmpty($response['errorCode']);
-                self::assertNotEmpty($response['errorMessage']);
-
-                if (empty($this->refund_request_id)) {
-                    $this->refund_request_id = $response['requestID'] ?? '';
-                }
-            }
-        }
-    }
-
-    /**
-     * @test
-     * @throws JsonException
-     */
-    final public function refund_request_details(): void
-    {
-        $paymentRefundDetailsRequestUrl = "https://sb-payments.ghoori.com.bd/api/v1.0/subscription/refund/{$this->refund_request_id}";
-        $paymentRefundDetailsRequest = PaymentRefundDetailsRequest::getInstance($paymentRefundDetailsRequestUrl, $this->fetch_access_token());
-        $response = $paymentRefundDetailsRequest->send();
-
-        self::assertNotEmpty($response);
-
-        self::assertArrayHasKey('requestID', $response);
-        self::assertArrayHasKey('amount', $response);
-        self::assertArrayHasKey('status', $response);
-        self::assertArrayHasKey('errorCode', $response);
-        self::assertArrayHasKey('errorMessage', $response);
-
-        self::assertNotEmpty($response['requestID']);
-        self::assertNotEmpty($response['amount']);
-        self::assertNotEmpty($response['status']);
-        self::assertNotEmpty($response['errorCode']);
-        self::assertNotEmpty($response['errorMessage']);
-    }
 
     private function fetch_access_token(): void
     {
