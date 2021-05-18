b89dd21f0e0f2a9309a0dbd401d4a1b0309a9a2f
peash1068 <peash1068@users.noreply.github.com>
2021-05-18 12:00:45 +0000

    Fix styling

--- tests/RefundRequestTests.php
+++ tests/RefundRequestTests.php
@@ -13,7 +13,6 @@ use PHPUnit\Framework\TestCase;
 
 class RefundRequestTests extends TestCase
 {
-
     public string $serverUrl = 'https://sb-payments.ghoori.com.bd';
     public string $tokenUrl = 'https://sb-payments.ghoori.com.bd/oauth/token';
     public string $username = 'demo@gmail.com';
@@ -29,16 +28,19 @@ class RefundRequestTests extends TestCase
      * @return array
      * @throws JsonException
      */
-    final public function fetch_payment_list($is_return = false): array {
+    final public function fetch_payment_list($is_return = false): array
+    {
         $paymentListRequestUrl = 'https://sb-payments.ghoori.com.bd/api/v1.0/subscription/310/payments'; //replace SERVER_URL & id (cancel request id) with value
         $paymentListRequest = PaymentListRequest::getInstance($paymentListRequestUrl, $this->fetch_access_token());
         $response = $paymentListRequest->send();
-        if($is_return) return $response['subscriptionPayments'];
+        if ($is_return) {
+            return $response['subscriptionPayments'];
+        }
 
         self::assertNotEmpty($response);
         self::assertArrayHasKey('subscriptionPayments', $response);
 
-        foreach ($response['subscriptionPayments'] as $subscriptionPayment){
+        foreach ($response['subscriptionPayments'] as $subscriptionPayment) {
             self::assertArrayHasKey('paymentId', $subscriptionPayment);
             self::assertArrayHasKey('cycle', $subscriptionPayment);
             self::assertArrayHasKey('dueDate', $subscriptionPayment);
@@ -50,6 +52,7 @@ class RefundRequestTests extends TestCase
             self::assertArrayHasKey('transactionDate', $subscriptionPayment);
             self::assertArrayHasKey('transactionId', $subscriptionPayment);
         }
+
         return [];
     }
 
@@ -57,11 +60,12 @@ class RefundRequestTests extends TestCase
      * @test
      * @throws JsonException
      */
-    final public function fetch_payment_details(): void{
+    final public function fetch_payment_details(): void
+    {
         $subscriptionPayments = $this->fetch_payment_list(true);
         self::assertNotEmpty($subscriptionPayments);
 
-        foreach ($subscriptionPayments as $subscriptionPayment){
+        foreach ($subscriptionPayments as $subscriptionPayment) {
             $paymentDetailsRequestUrl = "https://sb-payments.ghoori.com.bd/api/v1.0/subscription/payment/{$subscriptionPayment['paymentId']}";
             $paymentDetailsRequest = PaymentDetailsRequest::getInstance($paymentDetailsRequestUrl, $this->fetch_access_token());
             $paymentDetails = $paymentDetailsRequest->send();
@@ -78,18 +82,18 @@ class RefundRequestTests extends TestCase
             self::assertArrayHasKey('transactionDate', $paymentDetails);
             self::assertArrayHasKey('transactionId', $paymentDetails);
         }
-
     }
 
     /**
      * @test
      * @throws JsonException
      */
-    final public function send_refund_request(): void{
+    final public function send_refund_request(): void
+    {
         $subscriptionPayments = $this->fetch_payment_list(true);
         self::assertNotEmpty($subscriptionPayments);
 
-        foreach ($subscriptionPayments as $subscriptionPayment){
+        foreach ($subscriptionPayments as $subscriptionPayment) {
             $paymentRefundRequestUrl = "https://sb-payments.ghoori.com.bd/api/v1.0/subscription/payment/{$subscriptionPayment['paymentId']}/refund";
             $refund_amount = 1;
             $paymentRefundRequest = \Dotlines\GhooriSubscription\PaymentRefundRequest::getInstance($paymentRefundRequestUrl, $this->fetch_access_token(), $refund_amount);
@@ -97,21 +101,22 @@ class RefundRequestTests extends TestCase
 
             self::assertNotEmpty($response);
 
-            if(Carbon::now()->diffInDays($subscriptionPayment['transactionDate']) >= 15){
+            if (Carbon::now()->diffInDays($subscriptionPayment['transactionDate']) >= 15) {
                 self::assertArrayHasKey('errorCode', $response);
                 self::assertArrayHasKey('errorMessage', $response);
 
                 self::assertNotEmpty($response['errorCode']);
                 self::assertNotEmpty($response['errorMessage']);
-            }else{
+            } else {
                 self::assertArrayHasKey('requestID', $response);
                 self::assertArrayHasKey('amount', $response);
                 self::assertArrayHasKey('status', $response);
                 self::assertArrayHasKey('errorCode', $response);
                 self::assertArrayHasKey('errorMessage', $response);
 
-                if(empty($this->refund_request_id))
+                if (empty($this->refund_request_id)) {
                     $this->refund_request_id = $response['requestID'] ?? '';
+                }
             }
         }
     }
@@ -120,7 +125,8 @@ class RefundRequestTests extends TestCase
      * @test
      * @throws JsonException
      */
-    final public function refund_request_details(): void{
+    final public function refund_request_details(): void
+    {
         $paymentRefundDetailsRequestUrl = "https://sb-payments.ghoori.com.bd/api/v1.0/subscription/refund/{$this->refund_request_id}";
         $paymentRefundDetailsRequest = PaymentRefundDetailsRequest::getInstance($paymentRefundDetailsRequestUrl, $this->fetch_access_token());
         $response = $paymentRefundDetailsRequest->send();
@@ -147,5 +153,4 @@ class RefundRequestTests extends TestCase
 
         return $tokenResponse['access_token'];
     }
-
 }
